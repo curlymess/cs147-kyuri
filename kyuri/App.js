f5a79fe28f26b6650a602f34efeda26ddb60ff45
Tom Shahar <tomshahar@Toms-MBP-2.lan>
2022-11-20 10:28:19 -0800

    moved part of browse to its own component

--- kyuri/App.js
+++ kyuri/App.js
@@ -6,12 +6,12 @@ import Icons from './assets/Icons';
 import themes from './assets/Themes/themes';
 import { palette } from './assets/Themes/palette';
 import {Top} from './app/components';
-import { BrowseScreen } from './app/components';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//Browse page components
+import BrowseSearchBar from './app/components/BrowseSearchBar.js';
 
 
 function Feed() {
@@ -42,73 +42,12 @@ function Feed() {
   )
 }
 
-function Browse(){
-  const browseStyles = StyleSheet.create({
-    container: {
-      width: '100%',
-      position: 'relative',
-      top: -23,
-    },
-    searchContainer: {
-      width: '100%',
-      height: 46,
-
-    },
-    searchBar: {
-      height: 46,
-      backgroundColor: 'white',
-      borderRadius: 8,
-      marginHorizontal: 10,
-      borderWidth: 3,
-      borderColor: palette.darkBrown,
-      flexDirection: 'row',
-      alignItems: 'center',
-      overflow: 'hidden',
-
-    },
-    searchBarButton: {
-      marginLeft: 'auto',
-      height: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 44,
-    },
-    filterButton: {
-      backgroundColor: palette.cream,
-      height: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 44,
-      borderLeftWidth: 3,
-      borderColor: palette.darkBrown,
-      borderTopRightRadius: 6,
-      borderBottomRightRadius: 6,
-    },
-    input: {
-      fontSize: 16,
-      marginLeft: 10,
-    }
-  });
+function Browse() {
   return (
   <View style={styles.container}>
     <Top style={styles.head} />
-    <StatusBar style="auto" />
-    <View style={browseStyles.container}>
-      <View style={browseStyles.searchContainer}>
-        <View style={browseStyles.searchBar}>
-          <TextInput 
-            placeholder = "Search"
-            style={browseStyles.input}
-          ></TextInput>
-          <Pressable style={browseStyles.searchBarButton}>
-            <Ionicons name='search' size={28} color='black'></Ionicons>
-          </Pressable>
-          <Pressable style={browseStyles.filterButton}>
-            <Ionicons name='options-outline' size={28} color='black'></Ionicons>
-          </Pressable>
-        </View>
-      </View>
-    </View>
+    <StatusBar style="auto"/>
+    <BrowseSearchBar></BrowseSearchBar>
   </View>  
   );
 }
@@ -118,7 +57,7 @@ function Profile(){
     <View style={styles.container}>
       <Top style={styles.head} />
       <StatusBar style="auto" />
-  
+
       <View style ={styles.container}>
         <Text>Profile</Text>
       </View>
---
+++ kyuri/app/components/BrowseSearchBar.js
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { View, StyleSheet, Text, Pressable, TextInput, PlatformColor } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { palette } from '../../assets/Themes/palette';
+import {Top} from '../../app/components';
+import { StatusBar } from 'expo-status-bar';
+
+
+
+export default function BrowseSearchBar() {
+    let [fontsLoaded] = useFonts({
+        Rosmatika: require('../../assets/Fonts/RosmatikaRegular-BWA45.ttf'),
+      });
+      if (!fontsLoaded) return <AppLoading />;
+
+    return (
+        <View style={browseStyles.container}>
+          <View style={browseStyles.searchContainer}>
+            <View style={browseStyles.searchBar}>
+              <TextInput 
+                placeholder = "Search"
+                style={browseStyles.input}
+              ></TextInput>
+              <Pressable style={browseStyles.searchBarButton}>
+                <Ionicons name='search' size={28} color='black'></Ionicons>
+              </Pressable>
+              <Pressable style={browseStyles.filterButton}>
+                <Ionicons name='options-outline' size={28} color='black'></Ionicons>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+    );
+}
+const browseStyles = StyleSheet.create({
+    container: {
+      width: '100%',
+      position: 'relative',
+      top: -23,
+    },
+    searchContainer: {
+      width: '100%',
+      height: 46,
+
+    },
+    searchBar: {
+      height: 46,
+      backgroundColor: 'white',
+      borderRadius: 8,
+      marginHorizontal: 10,
+      borderWidth: 3,
+      borderColor: palette.darkBrown,
+      flexDirection: 'row',
+      alignItems: 'center',
+      overflow: 'hidden',
+
+    },
+    searchBarButton: {
+      marginLeft: 'auto',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 44,
+    },
+    filterButton: {
+      backgroundColor: palette.cream,
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 44,
+      borderLeftWidth: 3,
+      borderColor: palette.darkBrown,
+      borderTopRightRadius: 6,
+      borderBottomRightRadius: 6,
+    },
+    input: {
+      fontSize: 16,
+      marginLeft: 10,
+    }
+});
