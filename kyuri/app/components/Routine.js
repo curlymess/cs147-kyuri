ae5c9c73051e9e7eaea5463ef20d1060308a5160
curlymess <54120650+curlymess@users.noreply.github.com>
2022-11-28 14:29:09 -0800

    routine component

---
+++ kyuri/app/components/Routine.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
+import Top from './Top';
+import { palette } from '../../assets/Themes/palette';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Routine = () => 
+{
+  return (
+
+<View style={styles.container}>
+    <Top style={styles.head} />
+
+    <Text>Routine! </Text>
+</View>
+  );
+};
+
+export default Routine;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: palette.white,
+      },
+      head: {
+        flex: 1,
+      },
+    
+
+});
