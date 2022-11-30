e4a13cd422a2be8597cc6f2eb0ca712c8fbce010
Tom Shahar <tomshahar@DN0a1e2e14.SUNet>
2022-11-29 20:48:11 -0800

    product thumbnail progress

--- kyuri/app/components/ProductThumbnail.js
+++ kyuri/app/components/ProductThumbnail.js
@@ -14,7 +14,7 @@ export default function ProductThumbnail(props) {
         name: 'Cucumber Moisturizer',
         image: '../../assets/ProductIcons/product5.png',
         type: 'Moisturizer',
-        rating: 5,
+        rating: 4,
         usedBy: [
             '../../assets/Icons/iris.png',
             '../../assets/Icons/tom.png',
@@ -24,6 +24,7 @@ export default function ProductThumbnail(props) {
     let [fontsLoaded] = useFonts({
         Rosmatika: require('../../assets/Fonts/RosmatikaRegular-BWA45.ttf'),
         Monda: require('../../assets/Fonts/Monda-Regular.ttf'),
+        MondaBold: require('../../assets/Fonts/Monda-Bold.ttf'),
     });
     if (!fontsLoaded) return <AppLoading />;
 
@@ -33,29 +34,76 @@ export default function ProductThumbnail(props) {
                 <View style={styles.pedestalWrapper}>
                 </View>
                 <View style={styles.nameWrapper}>
-                    <Text>{data.name}</Text>
+                    <Text style={styles.name}>{data.name}</Text>
+                </View>
+            </View>
+            <View style={styles.right}>
+                <View style={styles.rightTop}>
+                    <Text style={styles.typeLabel}>Type </Text>
+                    <Text style={styles.type}>{data.type}</Text>
+                    <View style={styles.stars}>
+                        <Ionicons name="star" size={12} color='white'/>
+                        <Ionicons name="star" size={12} color='white'/>
+                        <Ionicons name="star" size={12} color='white'/>
+                        <Ionicons name="star" size={12} color='white'/>
+                        <Ionicons name="star-outline" size={12} color='white'/>
+
+                    </View>
                 </View>
             </View>
         </View>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
         marginHorizontal: 10,
         flexDirection: 'row',
         alignItems: 'center',
+        height: 64,
+        borderRadius: 12,
+        overflow: 'hidden',
     },
     pedestalWrapper: {
-        width: '50%',
+        width: '40%',
     },
     left: {
-        width: '40%',
+        width: '45%',
         backgroundColor: palette.cream,
+        height: '100%',
+        flexDirection: 'row',
+    },
+    right: {
+        width: '55%',
+        height: '100%',
+        backgroundColor: palette.darkBrown,
     },
     nameWrapper: {
-
+        width: '60%',
+        justifyContent: 'center',
+    },
+    name: {
+        fontFamily: 'MondaBold',
+    },
+    rightTop: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 5,
+    },
+    typeLabel: {
+        fontFamily: 'MondaBold',
+        color: 'white',
+        
+    },
+    type: {
+        color: 'white',
+        fontFamily: 'Monda',
+        marginTop: 0.5,
+    },
+    stars: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 'auto',
+        marginRight: 5,
     }
-
-
-
 });
