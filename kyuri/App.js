575357092986632646b435d118fd344441aceee9
curlymess <54120650+curlymess@users.noreply.github.com>
2022-12-05 14:43:19 -0800

    feed

--- kyuri/App.js
+++ kyuri/App.js
@@ -26,8 +26,6 @@ import Routine from './app/components/Routine';
 import EmptyRoutineCard from './app/components/EmptyRoutineCard';
 import RoutineCard from './app/components/RoutineCard';
 
-
-
 import NewPostsStatus from './app/components/NewPostsStatus.js';
 
 import FeedContent from './app/components/FeedContent';
@@ -101,6 +99,46 @@ function Browse({navigation}) {
 
 function Profile( {navigation, posts} )
 {
+  const imageSelect = (inputImg) => {
+  if (inputImg === null){
+    return Icons.tom;
+  }
+  const outputImg = {
+    'tom': Icons.tom,
+    'iris': Icons.iris,
+    'p1': Icons.p1,
+    'p2': Icons.p2,
+    'p3': Icons.p3,
+    'p4': Icons.p4,
+    'p5': Icons.p5,
+    'p6': Icons.p6,
+    'p7': Icons.p7,
+    'p8': Icons.p8,
+
+    'product1': Icons.product1,
+    'product2': Icons.product2,
+    'product3': Icons.product3,
+    'product4': Icons.product4,
+    'product5': Icons.product5,
+    'product6': Icons.product6,
+    'product7': Icons.product7,
+    'product8': Icons.product8,
+    'product9': Icons.product9,
+    'product10': Icons.product10,
+    'product11': Icons.product11,
+    'product12': Icons.product12,
+    'product13': Icons.product13,
+    'product14': Icons.product14,
+    'product15': Icons.product15,
+    'product16': Icons.product16,
+    'product17': Icons.product17,
+    'product18': Icons.product18,
+    'product19': Icons.product19,
+    'product20': Icons.product20,
+  };
+  return outputImg[inputImg];
+}
+
   const profileStyles = StyleSheet.create({
     myProfileCard: {
       flexDirection: 'row',
--- kyuri/app/components/FeedContent.js
+++ kyuri/app/components/FeedContent.js
@@ -22,9 +22,31 @@ const imageSelect = (inputImg) => {
     'p6': Icons.p6,
     'p7': Icons.p7,
     'p8': Icons.p8,
+
+    'product1': Icons.product1,
+    'product2': Icons.product2,
+    'product3': Icons.product3,
+    'product4': Icons.product4,
+    'product5': Icons.product5,
+    'product6': Icons.product6,
+    'product7': Icons.product7,
+    'product8': Icons.product8,
+    'product9': Icons.product9,
+    'product10': Icons.product10,
+    'product11': Icons.product11,
+    'product12': Icons.product12,
+    'product13': Icons.product13,
+    'product14': Icons.product14,
+    'product15': Icons.product15,
+    'product16': Icons.product16,
+    'product17': Icons.product17,
+    'product18': Icons.product18,
+    'product19': Icons.product19,
+    'product20': Icons.product20,
   };
   return outputImg[inputImg];
 }
+
 
 const renderPost = (item, navigation ) => {
   console.log(item.userImg);
